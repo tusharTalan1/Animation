66ff7ccacf3958aa1a3f1e07c16bed1afaaac564
tusharTalan1 <tushartalan10@gmail.com>
2026-09-05 10:25:09 +0530

    add the animation to the blackhole fpr faid trail effect

--- animation.js
+++ animation.js
@@ -6,12 +6,38 @@ canvas.width = windowWidth;
 canvas.height = windowHight;
 
 
+let speedx = 3;
+let speedy=3;
+let x = 99;
+let y = 99;
 
 
-context.beginPath();
-context.arc(780, 350, 100, 0, Math.PI * 2, false);
-context.lineWidth = 10;
-context.strokeStyle = "white";
-context.stroke();
-context.closePath();
+
+function animate() {
+    
+   
+    context.beginPath();
+    context.arc(x, y, 80, 0, Math.PI * 2, false);
+    context.lineWidth = 10;
+    context.strokeStyle = "white";
+    context.stroke();
+    context.closePath();
+    x += speedx;  
+    y += speedy;
+    
+    if (x + 80 > canvas.width || x - 80 < 0) {
+        speedx = -speedx;
+    }
+    if (y + 80 > canvas.height || y - 80 < 0) {
+        speedy = -speedy;
+    }
+
+    context.fillStyle = 'rgba(0, 0, 0, 0.1)'; 
+    context.fillRect(0, 0, canvas.width, canvas.height);
+    
+
+    requestAnimationFrame(animate);
+}
+
+animate();
 
