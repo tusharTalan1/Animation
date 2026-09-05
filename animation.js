d58ae41c444b186f0b467fe8e997a86930c01592
tusharTalan1 <tushartalan10@gmail.com>
2026-09-05 11:42:29 +0530

    add the code to make 500 stars at random positions using class

--- animation.js
+++ animation.js
@@ -7,37 +7,65 @@ canvas.height = windowHight;
 
 
 let speedx = 3;
-let speedy=3;
+let speedy = 3;
 let x = 99;
 let y = 99;
 
+class Star {
+    constructor(height, width){
+        this.radius = Math.random();
+        this.x = Math.random() * width;
+        this.y = Math.random() * height;
+    }
 
 
-function animate() {
-    
-   
+    makeStar(){
+        context.beginPath();
+        context.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+        context.fillStyle = "white";
+        context.fill();
+        context.closePath();
+    }
+}
+
+
+
+const stars = [];
+for (let i = 0; i < 500; i++){
+    stars.push(new Star(canvas.height, canvas.width));
+}
+
+
+function animate(){
+    context.fillStyle = 'rgba(0, 0, 0, 0.1)';
+    context.fillRect(0, 0, canvas.width, canvas.height);
+
+    stars.forEach(star =>{
+        star.makeStar();
+    });
+
     context.beginPath();
     context.arc(x, y, 80, 0, Math.PI * 2, false);
     context.lineWidth = 10;
     context.strokeStyle = "white";
     context.stroke();
     context.closePath();
-    x += speedx;  
-    y += speedy;
-    
-    if (x + 80 > canvas.width || x - 80 < 0) {
-        speedx = -speedx;
-    }
-    if (y + 80 > canvas.height || y - 80 < 0) {
-        speedy = -speedy;
-    }
 
-    context.fillStyle = 'rgba(0, 0, 0, 0.1)'; 
-    context.fillRect(0, 0, canvas.width, canvas.height);
-    
+    x += speedx;
+    y += speedy;
 
     requestAnimationFrame(animate);
 }
-
 animate();
+
+
+
+
+
+
+
+
+
+
+
 
